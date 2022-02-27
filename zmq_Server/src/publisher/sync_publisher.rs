b2517cfdcca50e04d80c18985e8dbb5270bb18db
Lizhuyou <84826607@qq.com>
2022-02-27 23:01:51 +0800

    优化

---
+++ zmq_Server/src/publisher/sync_publisher.rs
@@ -0,0 +1,29 @@
+
+pub fn new_sync_pub(){
+  let context = zmq::Context::new();
+
+    //socket to talk to clients
+    let publisher = context.socket(zmq::PUB).unwrap();
+    publisher.set_sndhwm(1_100_000).expect("failed setting hwm");
+    publisher
+        .bind("tcp://*:5561")
+        .expect("failed binding publisher");
+
+    //socket to receive signals
+    let syncservice = context.socket(zmq::REP).unwrap();
+    syncservice
+        .bind("tcp://*:5562")
+        .expect("failed binding syncservice");
+    //get syncronization from subscribers
+    for _ in 0..10 {
+        println!("Waiting for subscribers");
+        syncservice.recv_msg(0).expect("failed receiving sync");
+        syncservice.send("", 0).expect("failed sending sync");
+    }
+    //now broadcast 1M updates followed by end
+    println!("Broadcasting messages");
+    for _ in 0..1_000_000 {
+        publisher.send("Rhubarb", 0).expect("failed broadcasting");
+    }
+    publisher.send("END", 0).expect("failed sending end");
+}
